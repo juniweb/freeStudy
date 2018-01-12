ea741f73cdabf54e49c84502156ee2bf0e6e4587
juniweb <juniwebp@gmail.com>
2018-01-13 00:11:51 +0900

    인프런 ES6 강좌

---
+++ Javascript/inflearn_EcmaScript6/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused_vars" : 1,
+        "no-undef" : 1,
+        "no-console" : "off"
+
+
+    }
+};
