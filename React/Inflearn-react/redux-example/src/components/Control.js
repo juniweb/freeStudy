d6a90e658f6b68bb39cd31ff194ca5105ef14e10
juniweb <juniwebp@gmail.com>
2018-01-27 00:37:32 +0900

    인프런 React 강좌-Redux

---
+++ React/Inflearn-react/redux-example/src/components/Control.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  onPlus: PropTypes.func,
+  onSubtract: PropTypes.func,
+  onRandomizeColor: PropTypes.func
+};
+
+function createWarning(funcName) {
+  return () => console.warn(funcName, ' is not defined');
+}
+
+const defaultProps = {
+  onPlus: createWarning('onPlus'),
+  onSubtract: createWarning('onSubtract'),
+  onRandomizeColor: createWarning('onRandomizeColor')
+};
+
+export default class Control extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.onPlus}>+</button>
+        <button onClick={this.props.onSubtract}>-</button>
+        <button onClick={this.props.onRandomizeColor}>Randomize Color</button>
+      </div>
+    );
+  }
+}
+
+Control.propTypes = propTypes;
+Control.defaultProps = defaultProps;
---
+++ React/Inflearn-react/redux-example/src/components/Value.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  number: PropTypes.number
+};
+
+const defaultProps = {
+  number: -1
+};
+
+export default class Value extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.number}</h1>
+      </div>
+    );
+  }
+}
+
+Value.propTypes = propTypes;
+Value.defaultProps = defaultProps;
